932d5b3742579d1aff54aeda294b0b052acfd788
nabendu82 <nabendu.biswas@gmail.com>
2022-05-26 11:00:40 +0530

    Final Part

--- components/Toast.js
+++ components/Toast.js
@@ -1,7 +1,14 @@
-import { StyleSheet, Text, View, DeviceEventEmitter } from 'react-native'
-import React, { useEffect } from 'react'
+import { StyleSheet, Text, View, DeviceEventEmitter, TouchableOpacity } from 'react-native'
+import React, { useState, useEffect, useRef } from 'react'
+
+const colors = { info: 'orange', success: 'green', danger: 'red' }
 
 const Toast = () => {
+    const [message, setMessage] = useState(null);
+    const [messageType, setMessageType] = useState(null);
+    const [timeOut, setTimeOut] = useState(4000);
+    const timeOutRef = useRef(null);
+
     useEffect(() => {
         DeviceEventEmitter.addListener('SHOW_TOAST', onNewToast);
         return () => {
@@ -9,17 +16,42 @@ const Toast = () => {
         }
     }, [])
 
-    const onNewToast = data => {
-        console.log('onNewToast', data);
+    const closeToast = () => {
+        setMessage(null);
+        setTimeOut(4000);
+        clearInterval(timeOutRef.current);
     }
 
-    return (
-        <View>
-            <Text>Toast</Text>
+    useEffect(() => {
+        if(message){
+            timeOutRef.current = setInterval(() => {
+                if(timeOut === 0) closeToast();
+                else setTimeOut(prev => prev - 1000);
+            }, 1000)
+        }
+
+        return () => {
+            clearInterval(timeOutRef.current);
+        }
+    }, [message, timeOut])
+
+    const onNewToast = data => {
+        setMessage(data.message);
+        setMessageType(data.type);
+    }
+
+    return message && (
+        <View style={[{ backgroundColor: colors[messageType]}, styles.toastContainer]}>
+            <TouchableOpacity>
+                <Text style={styles.textStyle}>{message}</Text>
+            </TouchableOpacity>
         </View>
     )
 }
 
-export default Toast
+const styles = StyleSheet.create({
+    toastContainer: { position: 'absolute', left: '4%', right: '4%',  bottom: '4%', zIndex: 1 },
+    textStyle: { color: 'white', fontSize: 18, textAlign: 'center', padding: 14, fontWeight: 'bold' }
+})
 
-const styles = StyleSheet.create({})
+export default Toast
--- components/ToastMessages.js
+++ components/ToastMessages.js
@@ -7,7 +7,7 @@ const ToastMessages = () => {
     return (
         <>
             <Text style={styles.titleText}>Toast Messages</Text>
-            <Button title="success" onPress={() => toast.success({ message: 'Sucessful'})} />
+            <Button title="success" onPress={() => toast.success({ message: 'Sucessful' })} />
             <Button title="error" onPress={() => toast.danger({ message: 'Error'})} />  
             <Button title="info" onPress={() => toast.info({ message: 'Info'})} />
         </>
